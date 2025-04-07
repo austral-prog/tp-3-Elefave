639282bbf0185368b9f8a6b82570000763a375a8
Elefave <efavelukes@mail.austral.edu.ar>
2025-04-07 11:01:37 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,5 +1,29 @@
 def check_vowels():
     # Código a implementar utilizando input.
+    name = input("Insert name: ")
+    name = name.lower()
+    if "a" in name:
+        print("Contiene a: True")
+    else:
+        print("Contiene a: False")
+    if "e" in name:
+        print("Contiene e: True")
+    else:
+        print("Contiene e: False")
+    if "i" in name:
+        print("Contiene i: True")
+    else:
+        print("Contiene i: False")
+    if "o" in name:
+        print("Contiene o: True")
+    else:
+        print("Contiene o: False")
+    if "u" in name:
+        print("Contiene u: True")
+    else:
+        print("Contiene u: False")
+check_vowels()
+
 
 
 # Para verificar este ejercicio ejecutar el comando
